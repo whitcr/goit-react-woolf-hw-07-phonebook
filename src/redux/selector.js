33c2bdb7659d6718428a0cca2c903f98ea43ceab
whitcr <whitcr0k@gmail.com>
2024-05-10 16:34:32 +0200

    fixc

--- src/redux/selector.js
+++ src/redux/selector.js
@@ -1,19 +1,9 @@
-import { createSelector } from '@reduxjs/toolkit';
-
 export const selectFilters = state => state.filter;
 export const selectError = state => state.contacts.error;
 export const selectIsLoading = state => state.contacts.isLoading;
 
 export const selectContacts = state => {
-  return state.contacts.items;
+  return state.contacts.items.filter(contact =>
+    contact.name.toLowerCase().includes(state.filter.toLowerCase())
+  );
 };
-
-const selector = createSelector(
-  selectFilters,
-  selectContacts,
-  (filter, contacts) => {
-    return contacts.items.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase())
-    );
-  }
-);
